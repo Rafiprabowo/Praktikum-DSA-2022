ec08426a9cdf9bbeda877c113a74fbc281c41e15
Muhamad Rafi Prabowo <iydrafiprabowo@gmail.com>
2022-03-06 16:10:50 +0700

    update

---
+++ Pertemuan3/SourceCode/Latihan1/Tabung.java
@@ -0,0 +1,18 @@
+package latihan.data;
+
+public class Tabung {
+    public double phi = 3.14;
+    public double jariJari;
+    public double tinggi;
+    public Tabung(double r, double t){
+
+        this.jariJari = r;
+        this.tinggi = t;
+    }
+    public double hitungLuas(){
+        return 2*phi*jariJari*(tinggi+jariJari);
+    }
+    public double hitungVolume(){
+        return phi*jariJari*jariJari*tinggi;
+    }
+}
